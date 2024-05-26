a9653052f8820079997118510c894f8c671ec962
sgmryu <sgmryusei@gmail.com>
2024-05-26 01:10:20 +0000

    Hello world

--- structure/database_structure.sql
+++ structure/database_structure.sql
@@ -1,92 +1,92 @@
-drop database if exists ferreteria_mat ;
-create database ferreteria_mat ;
+DROP DATABASE IF EXISTS ferreteria_mat ;
+CREATE DATABASE IF NOT EXISTS ferreteria_mat ;
 
-use ferreteria_mat ;
+USE ferreteria_mat ;
 
-create table clientes (
-     id_clientes   int primary key auto_increment
-,    nombre        varchar (100)
-,    direccion     varchar (150)
-,    fecha         date
-,    tel           varchar (50)
-,    id_producto   int
-,    id_compra     int
-
-) ;
-create table compra (
-	 id_compra    int primary key auto_increment
-,    fecha        date
-,    total        decimal (20,0)
-,    id_producto  int
-,    id_venta     int
-,    id_clientes  int
+CREATE TABLE clientes (
+     id_clientes   INT PRIMARY KEY AUTO_INCREMENT
+,    nombre        VARCHAR(100)
+,    direccion     VARCHAR(150)
+,    fecha         DATE
+,    tel           VARCHAR(50)
+,    id_producto   INT
+,    id_compra     INT
 
 ) ;
-create table venta (
-	 id_venta    int primary key auto_increment
-,    fecha       date
-,    total       decimal (20,0)
-,    id_producto int
-,    id_compra   int
+CREATE TABLE compra (
+	 id_compra    INT PRIMARY KEY AUTO_INCREMENT
+,    fecha        DATE
+,    total        DECIMAL(20,0)
+,    id_producto  INT
+,    id_venta     INT
+,    id_clientes  INT
 
 ) ;
-create table producto (
-     id_producto  int primary key auto_increment
-,    nombre       varchar (200)
-,    descripcion  text
-,    precio       decimal (20,00)
-,    stock        int
-,    id_proveedor int
-,    id_clientes  int
-,    id_compra    int
-,    id_venta     int
+CREATE TABLE venta (
+	 id_venta    INT PRIMARY KEY AUTO_INCREMENT
+,    fecha       DATE
+,    total       DECIMAL(20,0)
+,    id_producto INT
+,    id_compra   INT
 
 ) ;
-create table proveedor (
-     id_proveedor  int primary key auto_increment
-,    nombre        varchar (200)
-,    dereccion     varchar (200)
-,    tel           varchar (50)
-,    mail          varchar (200)
-,    id_producto   int
-,    id_compra     int
+CREATE TABLE producto (
+     id_producto  INT PRIMARY KEY AUTO_INCREMENT
+,    nombre       VARCHAR(200)
+,    descripcion  TEXT
+,    precio       DECIMAL(20,00)
+,    stock        INT
+,    id_proveedor INT
+,    id_clientes  INT
+,    id_compra    INT
+,    id_venta     INT
+
+) ;
+CREATE TABLE proveedor (
+     id_proveedor  INT PRIMARY KEY AUTO_INCREMENT
+,    nombre        VARCHAR(200)
+,    dereccion     VARCHAR(200)
+,    tel           VARCHAR(50)
+,    mail          VARCHAR(200)
+,    id_producto   INT
+,    id_compra     INT
 ) ;
 
 
 -- fk tabla cliente
 
 alter table  clientes
-    add constraint fk_clientes_compra
+    add constraINT fk_clientes_compra
     foreign key (id_compra) references compra (id_compra) ;
 
 alter table  clientes
-    add constraint fk_clientes_producto
+    add constraINT fk_clientes_producto
     foreign key (id_producto) references producto (id_producto) ;
 
 -- fk compra
     
     alter table  compra
-    add constraint fk_compra_producto
+    add constraINT fk_compra_producto
     foreign key (id_producto) references producto (id_producto) ;
     
      alter table  compra
-    add constraint fk_compra_venta
+    add constraINT fk_compra_venta
     foreign key (id_venta) references venta (id_venta) ;
     
 -- fk venta
 
  alter table  venta
-    add constraint fk_venta_producto
+    add constraINT fk_venta_producto
     foreign key (id_producto) references producto (id_producto) ;    
     
 -- fk producto 
 
 alter table  producto
-    add constraint fk_producto_proveedor
+    add constraINT fk_producto_proveedor
     foreign key (id_proveedor) references proveedor (id_proveedor) ;     
     
 -- fk proveedor 
 
 alter table  proveedor
-    add constraint fk_proveedor_compra
+    add constraINT fk_proveedor_compra
     foreign key (id_compra) references compra (id_compra) ;
--- structure/population.sql
+++ structure/population.sql
@@ -1,4 +1,4 @@
-use ferreteria_mat;
+USE ferreteria_mat;
 
 
 
