783e039740a85413055b71eca5594b0ce4701d5e
Matias Reynoso <103078155+matiasreyno@users.noreply.github.com>
2024-05-27 01:53:31 +0000

    "cambios en database"

--- structure/database_structure.sql
+++ structure/database_structure.sql
@@ -1,5 +1,5 @@
- drop database if exists ferreteria_mat;
- create database ferreteria_mat;
+drop database if exists ferreteria_mat ;
+create database ferreteria_mat ;
 
 
  use ferreteria_mat ;
@@ -15,46 +15,46 @@ create table clientes (
 ) ;
 
 -- select * from ferreteria_mat.clientes ;
-create table compra (
-	 id_compra    int primary key not null auto_increment
-,    fecha        date
-,    total        decimal(20,0)
-,    id_producto  int
-,    id_venta     int
-,    id_clientes  int
+
+create table compra  (
+       id_compra     int primary key not null auto_increment 
+,      fecha         date 
+,      total         decimal(20,0) 
+,      id_clientes   int 
 
 ) ;
-create table venta (
-	 id_venta    int primary key auto_increment
-,    fecha       date
-,    total       decimal(20,0)
-,    id_producto int
-,    id_compra   int
 
-) ;
+-- select * from ferreteria_mat.compra ;
+
+
+create table venta  (
+	   id_venta      int primary key not null auto_increment 
+,      fecha         date 
+,      total         decimal(20,0)  
+,      id_compra     int
+)  ;
+
+
 create table producto (
-     id_producto  int primary key auto_increment
-,    nombre       varchar(200)
-,    descripcion  TEXT
-,    precio       decimal(20,00)
-,    stock        int
-,    id_proveedor int
-,    id_clientes  int
-,    id_compra    int
-,    id_venta     int
+       id_producto    int primary key not null auto_increment
+,      nombre         varchar(200) 
+,      precio         decimal(20,0) 
+,      stock          int 
+,      id_venta       int  
 
-) ;
-create table proveedor (
-     id_proveedor  int primary key auto_increment
-,    nombre        varchar(200)
-,    dereccion     varchar(200)
-,    tel           varchar(50)
-,    mail          varchar(200)
-,    id_producto   int
-,    id_compra     int
-) ;
+)  ;
+
+-- select * from ferreteria_mat.producto  ;
 
 
+create table proveedor  (
+	   id_proveedor     int primary key not null auto_increment
+,      nombre           varchar(200) 
+,      dereccion        varchar(200) 
+,      tel              varchar(50) 
+,      mail             varchar(200) 
+,      id_producto      int 
+)  ;
 
 create table historial_precios (
     id int auto_increment primary key,
@@ -65,41 +65,31 @@ create table historial_precios (
 );
 
 
+-- select * from ferreteria_mat.proveedor ;
 
--- fk tabla cliente
+-- fk de proveedor
 
-alter table  clientes
-   add constraint fk_clientes_compra
-    foreign key(id_compra) references compra(id_compra) ;
+alter table proveedor
+   add constraint __fk_proveedor_producto
+   foreign key (id_producto) references producto (id_producto) ;
+   
+-- fk de producto
+   
+alter table producto
+   add constraint __fk_producto_venta
+   foreign key (id_venta) references venta (id_venta) ;
+   
+-- fk de venta
 
-alter table  clientes
-   add constraint fk_clientes_producto
-    foreign key(id_producto) references producto(id_producto) ;
+alter table venta
+   add constraint __fk_venta_compra
+   foreign key (id_compra) references compra (id_compra) ;
+   
+-- fk de compra
 
--- fk compra
-    
-    alter table  compra
-   add constraint fk_compra_producto
-    foreign key(id_producto) references producto(id_producto) ;
-    
-     alter table  compra
-   add constraint fk_compra_venta
-    foreign key(id_venta) references venta(id_venta) ;
-    
--- fk venta
+alter table compra
+   add constraint __fk_compra_clientesPRIMARY
+   foreign key (id_clientes) references clientes (id_clientes) ;
+   
+alter table producto add column costo decimal(20, 2);
 
- alter table  venta
-   add constraint fk_venta_producto
-    foreign key(id_producto) references producto(id_producto) ;    
-    
--- fk producto 
-
-alter table  producto
-   add constraint fk_producto_proveedor
-    foreign key(id_proveedor) references proveedor(id_proveedor) ;     
-    
--- fk proveedor 
-
-alter table  proveedor
-   add constraint fk_proveedor_compra
-    foreign key(id_compra) references compra(id_compra) ;
