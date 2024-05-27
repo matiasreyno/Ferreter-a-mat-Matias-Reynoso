b97573dd0db15a11ef07cb5606dc69321b847621
Matias Reynoso <103078155+matiasreyno@users.noreply.github.com>
2024-05-27 20:41:58 +0000

    "modif en roles"

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -3,12 +3,19 @@ use ferreteria_mat ;
 create user 'gerente_de_venta_user'@'localhost' identified by 'gerente_password';
 create user 'cajero_user'@'localhost' identified by 'cajero_password';
 
-
 create role gerente_de_venta;
 
+-- Otorgar permisos de selección, inserción y actualización en todas las tablas de la base de datos ferreteria
+grant select, insert, update on ferreteria_mat.* to 'gerente_de_venta';
 
-grant select, insert, update on ferreteria_mat.* to 'gerente_de_ventas';
-grant create view on ferreteria_mat.* to 'gerente_de_ventas';
+-- Otorgar permisos para crear vistas en la base de datos ferreteria
+grant create view on ferreteria_mat.* to 'gerente_de_venta';
+
+-- create role gerente_de_venta;
+
+
+-- grant select, insert, update on ferreteria_mat.* to 'gerente_de_ventas';
+-- grant create view on ferreteria_mat.* to 'gerente_de_ventas';
 
 
 
