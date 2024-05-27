7f61862333b4c27c08e60df47b0134b61e4e183b
Matias Reynoso <103078155+matiasreyno@users.noreply.github.com>
2024-05-27 22:13:44 +0000

    "modif en roles"

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -1,16 +1,16 @@
 use ferreteria_mat ;
 
-create user 'gerente_de_venta_user'@'localhost' identified by 'gerente_password';
+-- create user 'gerente_de_venta_user'@'localhost' identified by 'gerente_password';
 
 create user 'cajero_user'@'localhost' identified by 'cajero_password';
 
-create role gerente_de_venta;
+-- create role gerente_de_venta;
 
 -- Otorgar permisos de selección, inserción y actualización en todas las tablas de la base de datos ferreteria
-grant select, insert, update on use_mysql.* to 'gerente_de_venta';
+-- grant select, insert, update on use_mysql.* to 'gerente_de_venta';
 
 -- Otorgar permisos para crear vistas en la base de datos ferreteria
-grant create view on usemysql.* to 'gerente_de_venta';
+-- grant create view on usemysql.* to 'gerente_de_venta';
 
 -- create role gerente_de_venta;
 
@@ -27,11 +27,11 @@ grant select, insert, update on ferreteria_mat.venta to 'cajero';
 
 
 
-grant 'gerente_de_venta' to 'gerente_de_venta_user'@'localhost';
+-- grant 'gerente_de_venta' to 'gerente_de_venta_user'@'localhost';
 grant 'cajero' to 'cajero_user'@'localhost';
 
 
-set default role gerente_de_venta to 'gerente_de_venta_user'@'localhost';
+-- set default role gerente_de_venta to 'gerente_de_venta_user'@'localhost';
 set default role cajero to 'cajero_user'@'localhost';
 
 flush privileges;
